3f4565ac0a74eaa7bdacbb25fd8d189d7069891f
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-01-19 16:50:46 +0000

    finalll

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 export function getPosts(searchQuery) {
-    const BASE_URL = "https://pixabay.com/api"; // Правильний базовий URL
+    const BASE_URL = "https://pixabay.com/api";
     const params = new URLSearchParams({
         key: "48269176-9eacf4bd75a8a580043143bd0",
         q: searchQuery,
@@ -8,14 +8,21 @@ export function getPosts(searchQuery) {
         safesearch: true
     });
 
-    const url = `${BASE_URL}?${params}`; // Формуємо правильний URL
+    const url = `https://api.allorigins.win/get?url=${encodeURIComponent('https://pixabay.com/api?' + params)}`;
 
     return fetch(url)
         .then(respond => {
-            if (!respond.ok) { // Перевірка статусу відповіді
+            if (!respond.ok) {
                 throw new Error(`HTTP error! status: ${respond.status}`);
             }
-            return respond.json();
+            return respond.json();  // Отримуємо JSON
+        })
+        .then(data => {
+            // Парсимо рядок JSON з поля 'contents'
+            const parsedData = JSON.parse(data.contents); // Парсимо contents як JSON
+            console.log('Parsed Data:', parsedData); // Перевірка результату
+
+            return parsedData;  // Повертаємо оброблені дані
         })
         .catch(error => {
             console.error(error);
